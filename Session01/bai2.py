59a22b720a6fed3e741fd2324b33d9c00ab4fe6c
Gia Bảo <m1procare@MacBookPro.lan>
2026-06-04 07:19:01 +0700

    up

---
+++ Session01/bai2.py
@@ -0,0 +1,9 @@
+print("-- HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN --")
+name_patient = str(input("Nhập tên của bệnh nhân: "))
+weight = float(input("Nhập cân nặng của bệnh nhân: "))
+print("-- KIỂM TRA DỮ LIỆU LƯU TRỮ --")
+print("Bệnh nhân: ", name_patient)
+print("Cân nặng đã nhập: ", weight)
+
+# Trưởng nhóm IT viết thêm dòng này để kiểm tra dữ liệu của cân nặng
+print("Cảnh báo - Kiểu dữ liệu đang lưu là: ", type(weight))
